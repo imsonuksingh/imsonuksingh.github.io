ea79af2f8edd7d04176d05bdd9dd019b77a45aa3
SONU KUMAR SINGH <sonusinghiitian@gmail.com>
2024-05-20 07:19:11 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Web Developer" , "Full Stack Developer" , "Java developer"],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
